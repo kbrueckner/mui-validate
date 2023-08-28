19e6a570efe50c30e9c209c1e83b509569b76aef
Brückner, Kenji <Kenji.Brueckner@prosiebensat1.com>
2023-08-28 15:56:27 +0200

    refactored type detection

--- src/fns/helper-fns.ts
+++ src/fns/helper-fns.ts
@@ -1,20 +1,30 @@
 import { InputType } from '../type';
 
 // eslint-disable-next-line
+const detectAutocomplete = (props: any): boolean => typeof props.autoComplete === 'boolean' || props.getOptionLabel !== undefined;
+
+// eslint-disable-next-line
+const detectPickerV5 = (props: any): boolean => [
+    props.allowKeyboardControl, props.KeyboardButtonProps, props.inputFormat,
+    props.mask, props.minDate, props.maxDate, props.disableMaskedInput, props.disableOpenPicker,
+    props.disableHighlightToday, props.desktopModeMediaQuery, props.defaultCalendarMonth,
+    props.allowSameDateSelection, props.onMonthChange, props.onYearChange, props.showDaysOutsideCurrentMonth,
+    props.OpenPickerButtonProps, props.renderDay, props.shouldDisableDate, props.shouldDisableYear,
+    props.showTodayButton, props.todayText,
+].some((val) => val !== undefined);
+
+// eslint-disable-next-line
 export const detectInputType = (props: any): InputType => {
-    if (typeof props.autoComplete === 'boolean' || props.getOptionLabel !== undefined) { return 'autocomplete'; }
-    if ([
-        // date picker v5
-        props.allowKeyboardControl, props.KeyboardButtonProps, props.inputFormat,
-        props.mask, props.minDate, props.maxDate, props.disableMaskedInput, props.disableOpenPicker,
-        props.disableHighlightToday, props.desktopModeMediaQuery, props.defaultCalendarMonth,
-        props.allowSameDateSelection, props.onMonthChange, props.onYearChange, props.showDaysOutsideCurrentMonth,
-        props.OpenPickerButtonProps, props.renderDay, props.shouldDisableDate, props.shouldDisableYear,
-        props.showTodayButton, props.todayText,
-        // date picker v6
-        // tbd ... e.g. props.slots,
-    ].some((val) => val !== undefined)) { return 'picker'; }
-    return 'textfield'; // select and textfield have the same behavior in the lib
+    if (
+        detectAutocomplete(props)
+    ) { return 'autocomplete'; }
+
+    if (
+        detectPickerV5(props)
+    ) { return 'picker'; }
+
+    // select and textfield remain but have the same behavior in the lib
+    return 'textfield';
 };
 
 // eslint-disable-next-line
